05bb405e121a601aeb508345a8ecdde5be0a198a
kareem <kareemwally@std.mans.edu.eg>
2024-11-12 12:10:45 +0200

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,31 @@
 #!/usr/bin/node
 const request = require('request');
+const util = require('util');
+const requestGet = util.promisify(request.get);
+
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-request.get(url, (error, response) => {
-  if (error) {
-    console.log(error);
-    console.log(response.statusCode);
-  }
-  if (!error && response.statusCode === 200) {
-    const characters = JSON.parse(response.body).characters;
-    for (const character of characters) {
-      request.get(character, (error, response) => {
-        if (error) { console.log(error); }
-        if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(response.body).name);
-        }
-      });
+
+async function getCharacterNames () {
+  try {
+    const response = await requestGet(url);
+    if (response.statusCode !== 200) {
+      throw new Error(`Failed to fetch movie data: ${response.statusCode}`);
     }
+
+    const characters = JSON.parse(response.body).characters;
+
+    for (const character of characters) {
+      const characterResponse = await requestGet(character);
+      if (characterResponse.statusCode !== 200) {
+        throw new Error(`Failed to fetch character data: ${characterResponse.statusCode}`);
+      }
+      const characterName = JSON.parse(characterResponse.body).name;
+      console.log(characterName);
+    }
+  } catch (error) {
+    console.error(error);
   }
-});
+}
+
+getCharacterNames();
